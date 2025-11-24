918840310fc8d100e43a3267e247d118726e0368
Taysa Abinader <ferreira.taysa@gmail.com>
2025-11-24 15:08:23 +0200

    [iter3] Understand Authentication Logic.

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -9,16 +9,20 @@ const requireAuth = async (req, res, next) => {
     return res.status(401).json({ error: "Authorization token required" });
   }
 
-//   console.log(authorization);
-//   console.log(authorization.split(" "));
-//   console.log(authorization.split(" ")[0]);
-//   console.log(authorization.split(" ")[1]);
+  console.log(authorization);
+  console.log(authorization.split(" "));
+  console.log(authorization.split(" ")[0]);
+  console.log(authorization.split(" ")[1]);
 
   const token = authorization.split(" ")[1];
 
   try {
     const { _id } = jwt.verify(token, process.env.SECRET);
 
+    //Iteration 3 (explanation):
+    //It finds the user ID on the database based on the token;
+    //Removes all extra fields (check only the ID);
+    //Attaches the user's ID to the request, which protects routes so you know who is making the request.
     req.user = await User.findOne({ _id }).select("_id");
     next();
   } catch (error) {
